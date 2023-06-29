db572f0cc0f7f7a1bb461b6683685bff088af005
PavelGith <linevich.p.l@gmail.com>
2023-06-29 21:10:29 +0300

    feat: add slide keyboard and media query

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,72 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 let pages = document.querySelectorAll(".page");
 let activePageArr = [0, 1];
-
-function activePages() {}
+let width1000 = window.matchMedia("(max-width: 1000px)");
 
 function pagesDisplayNone() {
   pages.forEach((element) => element.classList.add("displayNone"));
 }
 
+function prevPush() {
+  if (activePageArr.length === 1) {
+    if (activePageArr[0] === 1) return;
+    pagesDisplayNone();
+    activePageArr[0] -= 1;
+    document.getElementById(activePageArr[0]).classList.remove("displayNone");
+  }
+  if (activePageArr.length === 2) {
+    if (activePageArr[0] === 0) return;
+    pagesDisplayNone();
+    activePageArr[0] -= 2;
+    activePageArr[1] -= 2;
+    document.getElementById(activePageArr[0]).classList.remove("displayNone");
+    document.getElementById(activePageArr[1]).classList.remove("displayNone");
+  }
+}
+
+function nextPush() {
+  pagesDisplayNone();
+  if (activePageArr.length === 1) {
+    activePageArr[0] += 1;
+    document.getElementById(activePageArr[0]).classList.remove("displayNone");
+    console.log(999999);
+  }
+  if (activePageArr.length === 2) {
+    activePageArr[0] += 2;
+    activePageArr[1] += 2;
+    document.getElementById(activePageArr[0]).classList.remove("displayNone");
+    document.getElementById(activePageArr[1]).classList.remove("displayNone");
+  }
+}
+
+function myFunction(x) {
+  if (x.matches) {
+    document.getElementById(activePageArr[0]).classList.add("displayNone");
+    activePageArr.shift();
+  } else {
+    if (activePageArr[0] === 0 || activePageArr[0] % 2 === 0) {
+      activePageArr[1] = activePageArr[0] + 1;
+    } else {
+      activePageArr[0] -= 1;
+      activePageArr[1] = activePageArr[0] + 1;
+    }
+    document.getElementById(activePageArr[0]).classList.remove("displayNone");
+    document.getElementById(activePageArr[1]).classList.remove("displayNone");
+  }
+}
+
+myFunction(width1000);
+width1000.addListener(myFunction);
+
 prev.addEventListener("click", () => {
-  if (activePageArr[0] === 0) return;
-  pagesDisplayNone();
-  activePageArr[0] -= 2;
-  activePageArr[1] -= 2;
-  document.getElementById(activePageArr[0]).classList.remove("displayNone");
-  document.getElementById(activePageArr[1]).classList.remove("displayNone");
+  prevPush();
 });
+
 next.addEventListener("click", () => {
-  pagesDisplayNone();
-  activePageArr[0] += 2;
-  activePageArr[1] += 2;
-  document.getElementById(activePageArr[0]).classList.remove("displayNone");
-  document.getElementById(activePageArr[1]).classList.remove("displayNone");
+  nextPush();
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 37) prevPush();
+  if (event.keyCode === 39) nextPush();
 });
