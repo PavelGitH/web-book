007d6ccb61b572dafda56918e6ed1691751006b2
PavelGith <linevich.p.l@gmail.com>
2023-10-06 13:37:53 +0300

    feat: add page 54

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 let pages = document.querySelectorAll(".page");
-let activePageArr = [44, 45];
+let activePageArr = [54, 55];
 let width700 = window.matchMedia("(max-width: 700px)");
 
 function pagesDisplayNone() {
